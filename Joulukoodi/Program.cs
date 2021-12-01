2c3f26cb593d212df4d351489690b7642f8e1cb6
Juha Pitkänen <juha.pitkanen@digia.com>
2021-12-01 16:14:34 +0200

    Add project files.

---
+++ Joulukoodi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Joulukoodi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tehtavat tehtavat = new Tehtavat();
+            int[] teht1 = { 100, 200, 100,100 };
+
+            var tulos = tehtavat.Tehtava1(teht1);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Joulukoodi/Tehtavat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Joulukoodi
+{
+    public class Tehtavat
+    {
+        public List<string> Tehtava1(int[] luvut)
+        {
+            int edellinenSyvyys = 0;
+            string arvo = "";
+            List<string> tulos = new List<string>();
+
+            for (int i = 0; i < luvut.Length; i++)
+            {
+                if (i == 0)
+                {
+                    arvo = luvut[i] + " (N/A - no previous measurement)";
+                    tulos.Add(arvo);
+                    
+                }
+                else
+                {
+                    if (edellinenSyvyys < luvut[i])
+                    {
+                        arvo = luvut[i] + " (increased)";
+                        tulos.Add(arvo);
+                    }
+                    else
+                    {
+                        arvo = luvut[i] + " (decreased)";
+                        tulos.Add(arvo);
+                    }
+                }
+                edellinenSyvyys = luvut[i];
+            }
+
+            return tulos;
+        }
+    }
+}
